2a83d1a698e9bc94237a926be4ccb8cc2436479b
Oscar Vera López <oscar.veral@um.es>
2023-12-26 01:28:16 +0100

    Change on time accounting.

--- src/main.rs
+++ src/main.rs
@@ -29,13 +29,18 @@ fn main() {
         return;
     }
 
+    let mut elapsed_total = 0;
     for solver in solvers {
         let time = std::time::Instant::now();
         let solution = solver.0();
-        let elapsed = time.elapsed().as_nanos() as f64 / 1_000_000.0;
+        let elapsed = time.elapsed().as_nanos();
+        let elapsed_current = elapsed as f64 / 1_000_000.0;
+        elapsed_total += elapsed;
 
         println!("Problem: {}", solver.1);
         println!("Solution: {}", solution);
-        println!("Elapsed: {}ms", elapsed);
+        println!("Elapsed: {}ms", elapsed_current);
     }
+
+    println!("Total elapsed: {}ms", elapsed_total as f64 / 1_000_000.0);
 }
